9ba1e6d95ab88ebe6f9249a332f85f93a979746f
ksh009 <khughessean01@gmail.com>
2024-06-04 10:35:49 +0200

    remove files not needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 
-// import { routes } from './app.routes';
-
 @NgModule({
   declarations: [
     AppComponent,
