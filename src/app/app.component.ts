566103ad1d400595940b82d852bc68a5af5ce9f9
ksh009 <khughessean01@gmail.com>
2024-06-04 13:24:10 +0200

    setup tailwind theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import "../styles.css";
 
 @Component({
   selector: 'app-root',
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts}", "./src/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: 'hsl(209, 23%, 22%)',
+        veryDarkBlueDM: 'hsl(207, 26%, 17%)',
+        veryDarkBlueLM: 'hsl(200, 15%, 8%)',
+        darkGray: 'hsl(0, 0%, 52%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+        white: 'hsl(0, 0%, 100%)',
+      },
+      fontFamily: {
+        sans: ['Nunito Sans', 'sans-serif'],
+      },
+      fontSize: {
+        bodyHomepage: '14px',
+        bodyDetailPage: '16px',
+      },
+    },
   },
   plugins: [],
-}
+};
