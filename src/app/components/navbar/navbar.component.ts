ec84d4fe1a17afc65f5a7e4a9c0d0f4a1bd33eae
ksh009 <khughessean01@gmail.com>
2024-06-04 14:17:34 +0200

    update theme switch color toggle

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,21 +1,42 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrl: "./navbar.component.css",
 })
 export class NavbarComponent {
-  themeMode: string = 'dark';
+  themeMode: string = "dark";
   themeModeStyles: ThemeModeStyles = {
-    backGroundComponents: 'darkBlue',
-    backGroundLayout: 'veryDarkBlueDM',
-    fontColorPrimary: 'white'
+    backGroundComponents: "darkBlue",
+    backGroundLayout: "veryDarkBlueDM",
+    fontColorPrimary: "white",
   };
+
+  toggleThemeMode() {
+    this.themeMode = this.themeMode === "dark" ? "light" : "dark";
+    this.updateThemeStyles();
+  }
+
+  updateThemeStyles() {
+    if (this.themeMode === "light") {
+      this.themeModeStyles = {
+        backGroundComponents: "white",
+        backGroundLayout: "veryLightGray",
+        fontColorPrimary: "veryDarkBlueLM",
+      };
+    } else {
+      this.themeModeStyles = {
+        backGroundComponents: "darkBlue",
+        backGroundLayout: "veryDarkBlueDM",
+        fontColorPrimary: "white",
+      };
+    }
+  }
 }
 
 interface ThemeModeStyles {
-  backGroundComponents: string,
-  backGroundLayout: string,
-  fontColorPrimary: string
+  backGroundComponents: string;
+  backGroundLayout: string;
+  fontColorPrimary: string;
 }
