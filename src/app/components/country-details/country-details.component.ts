a977c863adecad565d60fb2b42f70f5dd36ac600
ksh009 <khughessean01@gmail.com>
2024-06-06 15:37:03 +0200

    get name from url params

--- src/app/components/country-details/country-details.component.ts
+++ src/app/components/country-details/country-details.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ThemeDataService } from '../../services/theme-data/theme-data.service';
+
+// This info will need to be in a shared service. Origin will be the GET service then distribute. 
+import data from '../../../assets/cleaned_data.json';
 
 @Component({
   selector: 'app-country-details',
-  standalone: true,
-  imports: [],
   templateUrl: './country-details.component.html',
-  styleUrl: './country-details.component.css'
+  styleUrls: ['./country-details.component.css']
 })
-export class CountryDetailsComponent {
+export class CountryDetailsComponent implements OnInit {
+  themeMode: string = "dark";
+  country: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private themeDataService: ThemeDataService
+  ) {}
+
+  // ngOnInit() {
+  //   this.themeDataService.themeMode$.subscribe((mode) => {
+  //     this.themeMode = mode;
+  //   });
+
+  //   const countryName = this.route.snapshot.paramMap.get('name');
+  //   this.country = data.find((item) => item.common_name.toLowerCase() === countryName.toLowerCase());
+  // }
+
+  ngOnInit() {
+    this.themeDataService.themeMode$.subscribe((mode) => {
+      this.themeMode = mode;
+    });
+  
+    const countryName = this.route.snapshot.paramMap.get('name');
+    if (countryName) {
+      this.country = data.find((item) => item.common_name.toLowerCase() === countryName.toLowerCase());
+    } else {
+      // Handle the case where countryName is null
+      console.log("No country name in params!!!!!")
+    }
+  }
+  
 }
