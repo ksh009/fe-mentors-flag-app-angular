fcd3a3649fa4fa0fffa3a94cbeae5331a8e0bfd3
ksh009 <khughessean01@gmail.com>
2024-06-13 14:21:07 +0200

    fix clashing tests - verify navigation end logic - pass

--- src/app/components/country-details/country-details.component.spec.ts
+++ src/app/components/country-details/country-details.component.spec.ts
@@ -3,9 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CountryDetailsComponent } from './country-details.component';
 import { AppModule } from '../../app.module';
 import { ActivatedRoute, convertToParamMap, Router, NavigationEnd } from '@angular/router';
-import { Location } from '@angular/common';
 import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs';
+import { Location } from '@angular/common';
+import { of, Subject } from 'rxjs';
 import { ThemeDataService } from '../../services/theme-data/theme-data.service';
 import { FlagsApiService } from '../../services/flags-api/flags-api.service';
 import { Country } from '../../interfaces/Country';
@@ -17,6 +17,8 @@ describe('CountryDetailsComponent', () => {
   let mockFlagsApiService: Partial<FlagsApiService>;
   let location: Location;
   let router: Router;
+  // UPDATE ANY *********************
+  const routerEventsSubject = new Subject<any>();
   const mockCountry: Country =     {
     common_name: "Algeria",
     official_name: "People's Democratic Republic of Algeria",
@@ -42,14 +44,14 @@ describe('CountryDetailsComponent', () => {
 
   beforeEach(async () => {
     mockFlagsApiService = {
-      getCountry: jasmine.createSpy().and.callFake((name: string) => {
+      getCountry: jasmine.createSpy().and.callFake(() => {
         return of(mockCountry);
       })
     };
 
     await TestBed.configureTestingModule({
       declarations: [CountryDetailsComponent],
-      imports: [AppModule],
+      imports: [AppModule, RouterTestingModule],
       providers: [
         ThemeDataService,
         { provide: FlagsApiService, useValue: mockFlagsApiService },
@@ -57,6 +59,13 @@ describe('CountryDetailsComponent', () => {
           provide: ActivatedRoute,
           useValue: {
             snapshot: { paramMap: convertToParamMap({ name: 'Algeria' }) }
+          }
+        },
+        {
+          provide: Router,
+          useValue: {
+            events: routerEventsSubject.asObservable(),
+            navigate: jasmine.createSpy('navigate')
           }
         }
       ],
@@ -69,6 +78,10 @@ describe('CountryDetailsComponent', () => {
     location = TestBed.inject(Location);
     router = TestBed.inject(Router);
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 
   it('should create', () => {
@@ -104,10 +117,18 @@ describe('CountryDetailsComponent', () => {
   });
 
   it('should navigate to country details when goToDetails(countryName) is called', () => {
-    const routerNavigateSpy = spyOn(router, 'navigate');
-
     component.goToDetails('Algeria');
 
-    expect(routerNavigateSpy).toHaveBeenCalledWith(['/country', 'Algeria']);
+    expect(router.navigate).toHaveBeenCalledWith(['/country', 'Algeria']);
+  });
+
+  it('should fetch country details on NavigationEnd event', () => {
+    const getCountryDetailsSpy = spyOn(component, 'getCountryDetails').and.callThrough();
+
+    routerEventsSubject.next(new NavigationEnd(1, '/country/Algeria', '/country/Algeria'));
+    fixture.detectChanges();
+
+    expect(getCountryDetailsSpy).toHaveBeenCalled();
+    expect(component.country).toEqual(mockCountry);
   });
 });
--- src/app/components/country-details/country-details.component.ts
+++ src/app/components/country-details/country-details.component.ts
@@ -21,8 +21,10 @@ export class CountryDetailsComponent implements OnInit {
     private location: Location,
     private router: Router,
   ) {
-    this.router.events.subscribe(() => {
-      this.getCountryDetails();
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.getCountryDetails();
+      }
     });
   }
 
